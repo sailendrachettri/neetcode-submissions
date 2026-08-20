d92acc9dc8360062c53e0c8bbd4ed8f279177d93
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-08-20 07:42:37 +0530

    Add: buy-two-chocolates - submission-0

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+
+        int res = money - (prices[0]+prices[1]);
+
+        return res >= 0 ? res : money;
+    }
+};
