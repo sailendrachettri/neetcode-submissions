4c9da1f7adfbb3aba3d058f6fde0d8d14720eb05
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-08-30 22:14:13 +0530

    Add: length-of-last-word - submission-1

---
+++ Data Structures & Algorithms/length-of-last-word/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int idx = s.size()-1;
+        int count = 0;
+
+        while(idx > 0 && s[idx] == ' '){
+            idx--;
+        }
+
+        while(idx > 0 && s[idx] != ' '){
+            idx--;
+            count++;
+        }
+
+        return count;
+    }
+};
