b2ceb9857721fa67ce08545d937d008e8e174d54
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-09-01 06:42:21 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == val){
+                nums.erase(nums.begin()+i);
+
+                if(i > 0) i--;
+            }
+        }
+
+        return nums.size();
+    }
+};
