a41b94d377aa3b1d07bce45e9861e2495159030f
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-09-02 06:53:35 +0530

    Add: ugly-number - submission-5

---
+++ Data Structures & Algorithms/ugly-number/submission-5.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool prime(int n){
+        bool isPrime = true;
+
+        if(n <= 1) return false;
+        else if(n == 2 || n == 3) return true;
+
+        for(int i = 2; i <= n/2; i++){
+            if(n%i == 0) return false;
+        }
+
+        return true;
+    }
+    bool isUgly(int n) {
+        set<int> st;
+
+        if(n <= 1) return true;
+
+        if(prime(n)) return false;
+
+
+        for(int i = 2; i <= n; i++){
+            bool res = prime(i);
+            
+            if(res){
+                if(!(i == 2 || i == 3 || i == 5)) return false;
+            }
+        }
+
+        return true;
+
+    }
+};
