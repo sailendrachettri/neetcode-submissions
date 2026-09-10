b5205f7d9e42d3d67471de9b6b260eec69d17ad7
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-09-10 07:24:27 +0530

    Add: 4sum - submission-0

---
+++ Data Structures & Algorithms/4sum/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        set<vector<int>> st;
+
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i+1; j < nums.size(); j++){
+                for(int k = j+1; k < nums.size(); k++){
+                    for(int l = k+1; l < nums.size(); l++){
+                        if(nums[i]+nums[j] + nums[k] + nums[l] == target){
+                            ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        }
+                    }
+                }
+            }
+        }
+
+        for(vector<int> &arr : ans){
+            sort(arr.begin(), arr.end());
+            st.insert(arr);
+        }
+
+        ans.clear();
+        for(vector<int> arr : st) ans.push_back(arr);
+
+
+
+        return ans;
+    }
+};
